617cc3fb734297bedef5594be3ecac7bfda72711
Anvi <anveshgouds@hotmail.com>
2025-05-14 11:39:49 +0530

    grammer check

--- GenAI/Programs/Main/LC_01-BasicTemplates.py
+++ GenAI/Programs/Main/LC_01-BasicTemplates.py
@@ -1,22 +1,26 @@
-from Models.llm import get_Gemini_model, llm
-#from langchain_google_genai import systempromptmessage, humanpromptmessage
-from langchain_core.prompts import PromptTemplate
-from langchain.chains import LLMChain
-
+from Models.llm import llm
 
 template = "You are a helpful assistant that translates {input_language} to {output_language}."
 human_template = "{text}"
 
-
 chat_prompt = llm.get_ChatPromptTemplate(template, human_template)
 
-messages = chat_prompt.format_messages(input_language="English", output_language="French", text="I love programming.")
+#messages = chat_prompt.format_messages(input_language="English", output_language="French", text="I love programming.")
 
 gemini_model = llm.get_Gemini_model()
 
 chain = chat_prompt | gemini_model # LLMChain(llm=llm, prompt=messages)
 
-response = chain.invoke({"text": "I love programming.", "input_language": "English", "output_language": "French"})
-print(response)
+response = chain.invoke({"text": "I love programming.", "input_language": "English", "output_language": "French, Hindi"})
+print(response.content)
+
+system_Text_GrammerCheck = "You are a grammar checker. Correct the grammar in the following text."
+human_Text_GrammerCheck = "Correct the grammar in the following text: {text}"
+chat_prompt_GrammerCheck = llm.get_ChatPromptTemplate(system_Text_GrammerCheck, human_Text_GrammerCheck)
+
+GrammerChecker = chat_prompt_GrammerCheck | gemini_model 
+
+response = GrammerChecker.invoke({"text": "Get out house my you"})
+print(response.content)
 
 
