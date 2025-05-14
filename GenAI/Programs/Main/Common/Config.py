44a117dca06d8cf9c02e1bd9615ba398410e557d
Anvi <anveshgouds@hotmail.com>
2025-05-14 21:54:24 +0530

    PDF Reader

--- GenAI/Programs/Main/Common/Config.py
+++ GenAI/Programs/Main/Common/Config.py
@@ -7,4 +7,8 @@ os.environ["GOOGLE_API_KEY"] = getpass("AIzaSyCl6tQ6NUrPDLtav7_JOF5Vmy9x4gfPt20"
 os.environ["LANGCHAIN_API_KEY"] = getpass("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")
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] =os.path.join(
     os.path.dirname(__file__), "../../Resources/googleCreds.json")
+os.environ["USER_AGENT"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+(KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.0.0"
+
+
 
--- GenAI/Programs/Main/Common/Database.py
+++ GenAI/Programs/Main/Common/Database.py
@@ -1,21 +1,14 @@
-
-
 from langchain_community.utilities import SQLDatabase
 from sqlalchemy import create_engine
 
-def get_database_connection(server, database, username, password):
-    """
-    Creates and returns a SQLAlchemy engine and SQLDatabase object.
 
-    Args:
-        server (str): The database server name.
-        database (str): The database name.
-        username (str): The username for authentication.
-        password (str): The password for authentication.
+"""
+    Creates and returns a SQLAlchemy engine and SQLDatabase object.
 
     Returns:
         SQLDatabase: A LangChain SQLDatabase object.
-    """
+"""
+def get_database_connection(server, database, username, password):   
     connection_string = (
         f"mssql+pyodbc://{username}:{password}@{server}/{database}?"
         "driver=ODBC Driver 17 for SQL Server"
@@ -23,13 +16,8 @@ def get_database_connection(server, database, username, password):
     engine = create_engine(connection_string)
     return SQLDatabase(engine)
 
-def get_db():
-    """
-    Returns a SQLAlchemy engine and SQLDatabase object.
 
-    Returns:
-        SQLDatabase: A LangChain SQLDatabase object.
-    """
+def get_db():    
     SERVER = 'ANVIRYZEN\\SQLEXPRESS'
     DATABASE = 'BlogDB'
     USERNAME = 'sa'
