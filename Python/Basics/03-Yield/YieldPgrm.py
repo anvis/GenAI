a179d73abfa2b0d69b6939811d4dd30345780e8a
Anvi <anveshgouds@hotmail.com>
2025-07-13 20:33:26 +0530

    yield

--- Python/Basics/03-Yield/YieldPgrm.py
+++ Python/Basics/03-Yield/YieldPgrm.py
@@ -20,8 +20,17 @@ generator = fetch_generator()
 print(generator)
 print(next(generator))  # Fetch the first line
 print(next(generator))  # Fetch the second line 
-print(list(generator))  # Fetch the remaining lines
+#print(list(generator))  # Fetch the remaining lines
 
+print("Using yield to fetch chunks of data")
 
+def fetch_chunk():
+    with open(os.path.dirname(__file__) + '/data.txt', 'r') as file:
+        while chunk := file.read(2):
+            yield chunk
 
+chunck = fetch_chunk()
+print(chunck)
+print(next(chunck))  # Fetch the first chunk
+print(next(chunck))  # Fetch the second chunk
     
